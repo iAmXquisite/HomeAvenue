51af5e733403db50e5f73b99b56a31f72ec40396
Christopher Ortiz <chris@Christophers-MacBook-Pro.local>
2017-11-01 19:57:36 -0700

    Email ready for testing

--- contact.php
+++ contact.php
@@ -1,28 +1,14 @@
 <?php
 
-$user_input = $_POST;
-// $errors[] = "";
-//
-// if ($user_input["name"] == ""){
-//   $errors[] = "Name is missing.";
-// }
-// if ($user_input["email"] == ""){
-//   $errors[] = "Email is missing.";
-// }
-// if ($user_input["subject"] == ""){
-//   $user_input["subject"] = "New Home Avenue Contact Message!";
-// }
-// if ($user_input["message"] == ""){
-//   $errors[] = "There has to be a message.";
-// }
-//
-// foreach($errors as $error) {
-//   print $error . "<br>";
-// }
-//
-// var_dump($user_input);
-
 require_once 'vendor/autoload.php';
+
+// Declare POST variables
+$sendTo = 'c.ortiz1216@gmail.com'
+$input = $_POST;
+$name = $input["name"];
+$email = $input["email"];
+$subject = $input["subject"]
+$body = $input["message"];
 
 // Create the Transport
 $transport = new Swift_MailTransport();
@@ -32,16 +18,16 @@ $mailer = new Swift_Mailer($transport);
 
 // Create a message
 $message = new Swift_Message();
-$message->setTo([ 'c.ortiz1216@gmail.com' => 'Home Avenue']);
-$message->setSubject("Home Avenue Contact Form");
-$message->setBody("You're our best client ever.");
-$message->setFrom("c.ortiz@lausd.net", "John Doe");
+$message->setTo([ $sendTo => 'Home Avenue']);
+$message->setSubject($subject);
+$message->setBody($body);
+$message->setFrom($email, $name);
 
 $result = $mailer->send($message);
 
 if ($result == 0){
-  echo "error";
+  echo "There was an error sending your email!";
 }else{
-  echo "success";
+  echo "Your email was sent successfully!";
 }
 ?>
